1cf081d2ed45a513d94c6e206f2a15c3df13085a
Daniel Beck <dbeck1801@gmail.com>
2026-03-17 10:45:42 +0200

    Created Like Class

--- Development/Blog_Project/likes/models.py
+++ Development/Blog_Project/likes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings 
+from posts.models import Post # Import Post model, each post has likes
 
-# Create your models here.
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes') # Likes belong to Post
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='likes') # User Likes the post
+
+    def __str__(self) -> str: # Controls what prints when you print a Like class
+        return f"{self.user.username} liked {self.post.title}"
