84b473abf17823f9d58c8a0fcb9386ef225fb8e2
Daniel Beck <dbeck1801@gmail.com>
2026-03-17 10:05:52 +0200

    created Post class

--- Development/Blog_Project/posts/models.py
+++ Development/Blog_Project/posts/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.conf import settings # imports the settings so we can reference AUTH_USER_MODEL
 
-# Create your models here.
 
+class Post(models.Model):
+    author : str = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts') # author(User) can have many posts that what ForeignKey means
+    title : str = models.CharField(max_length=255) # Short text for title
+    content : str = models.TextField() # Unlimited text for body
+    created_at = models.DateTimeField(auto_now_add=True) # The time now
+    updated_at = models.DateTimeField(auto_now=True) # Updates every time
+
+    def __str__(self) -> str: # Controls what prints when you print a Post class
+        return self.title
