81e7df69f4399ba687fd08917d566a0e19048166
Daniel Beck <dbeck1801@gmail.com>
2026-03-17 10:36:17 +0200

    Comment Class created

--- Development/Blog_Project/comments/models.py
+++ Development/Blog_Project/comments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from posts.models import Post # Import Post Model, each post has comments
 
-# Create your models here.
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments') # Comment belong to Post
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments') # User writes the Comment
+    content : str = models.TextField() # Unlimited field to write the body
+    created_at = models.DateTimeField(auto_now_add=True) # Date and time at the moment of creation
+
+    def __str__(self) -> str: # Controls what prints when you print a Post class
+        return f"{self.author.username} on {self.post.title}" 
