1846c0842d76ca97adefba04e1a448fffd4acca1
Daniel Beck <dbeck1801@gmail.com>
2026-04-06 17:16:09 +0300

    all comments endpoints connected to the posts endpoints

--- Development/Blog_Project/comments/views.py
+++ Development/Blog_Project/comments/views.py
@@ -94,7 +94,7 @@ class CommentDetailView(APIView):
         summary="Delete a comment",
         tags=["Comments"]
     )
-    def delete(self, request, ost_id: int ,id: int) -> JsonResponse:
+    def delete(self, request, post_id: int ,id: int) -> JsonResponse:
         """Delete a comment — only the author can do this."""
         comment = self.get_comment(id)
         if not comment:
